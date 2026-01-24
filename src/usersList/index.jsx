f0c40421025bff1628b7afe600918d51a9bb4651
Muxriddin Toshboyev <167794484+MukhriddinToshboyev@users.noreply.github.com>
2026-01-24 16:11:05 +0500

    refactor: remove unused user details from Users component and adjust user-card styling

--- src/usersList/index.jsx
+++ src/usersList/index.jsx
@@ -6,12 +6,6 @@ function Users({ users, HandleClick }) {
       {users.map((user) => {
         return (
           <div key={user.id} className="user-card">
-            <p className="userText">{user.name}</p>
-            {/* <p className="userText">{user.email}</p>
-            <p className="userText">{user.phone}</p>
-            <p className="userText">{user.website}</p>
-            <p className="userText">{user.company.name}</p>
-            <p className="userText">{user.address.city}</p> */}
             <button onClick={() => HandleClick(user.id)}>Delete</button>
           </div>
         );
