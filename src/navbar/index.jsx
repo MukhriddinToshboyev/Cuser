9625724b7dc2493232c242e1e3559b973269d661
Muxriddin Toshboyev <167794484+MukhriddinToshboyev@users.noreply.github.com>
2026-01-26 15:15:11 +0500

    style: adjust Navbar height and improve modal positioning

--- src/navbar/index.jsx
+++ src/navbar/index.jsx
@@ -2,7 +2,7 @@ import "./style.css";
 function Navbar({ user }) {
   return (
     <div className="Navbar-container">
-      <h1>Cuser</h1>
+      <h1 className="Navbar-title">Cuser</h1>
       <h1>
         {" "}
         {user.length > 0 ? "You have:" + " " + user.length : "No users :("}
