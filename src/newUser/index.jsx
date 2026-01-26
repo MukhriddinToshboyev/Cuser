8efe02e7c0efd060102219b3c266ab3adc49b661
Muxriddin Toshboyev <167794484+MukhriddinToshboyev@users.noreply.github.com>
2026-01-26 14:43:18 +0500

    feat: enhance New User modal with additional fields and improved styling

--- src/newUser/index.jsx
+++ src/newUser/index.jsx
@@ -5,16 +5,43 @@ function NewUser() {
     <div className="modalContainer">
       <div className="modal">
         <h2>Create New User</h2>
-        <form>
+        <form className="modalForm">
           <label>
-            <span> Name:</span>
-            <input type="text" placeholder="Enter name" />
+            <span className="modalText"> Img URL:</span>
+            <input type="url" required className="modalInput" />
           </label>
           <label>
-            <span> Email:</span>
-            <input type="email" placeholder="Enter email" />
+            <span className="modalText"> First Name:</span>
+            <input type="text" required className="modalInput" />
           </label>
-          <button type="submit">Add User</button>
+          <label>
+            <span className="modalText"> Last Name:</span>
+            <input type="text" required className="modalInput" />
+          </label>
+          <label>
+            <span className="modalText"> Age:</span>
+            <input type="number" required className="modalInput" />
+          </label>
+          <label>
+            <span className="modalText"> From:</span>
+            <input type="text" required className="modalInput" />
+          </label>
+          <label>
+            <span className="modalText"> Job:</span>
+            <input type="text" required className="modalInput" />
+          </label>
+          <div className="gender">
+            <span className="modalText"> Gender:</span>
+            <label>
+              <input type="radio" name="gender" value="male" required />
+              Male
+            </label>
+            <label>
+              <input type="radio" name="gender" value="female" required />
+              Female
+            </label>
+          </div>
+          <button className="modalButton">Submit</button>
         </form>
       </div>
     </div>
