8818a80a12f1329eeed03ed65c0f802911170435
Muxriddin Toshboyev <167794484+MukhriddinToshboyev@users.noreply.github.com>
2026-01-26 18:15:54 +0500

    feat: implement user state management and input handling in NewUser component

--- src/newUser/index.jsx
+++ src/newUser/index.jsx
@@ -1,6 +1,16 @@
+import { useState } from "react";
 import "./style.css";
 
 function NewUser() {
+  const [user, setUser] = useState({
+    img: "",
+    firstName: "",
+    lastName: "",
+    age: "",
+    from: "",
+    job: "",
+    gender: "",
+  });
   return (
     <div className="modalContainer">
       <div className="modal">
@@ -8,36 +18,110 @@ function NewUser() {
         <form className="modalForm">
           <label>
             <span className="modalText"> Img URL:</span>
-            <input type="url" required className="modalInput" />
+            <input
+              onChange={(e) => {
+                setUser((prev) => {
+                  return { ...prev, img: e.target.value };
+                });
+              }}
+              type="url"
+              required
+              className="modalInput"
+            />
           </label>
           <label>
             <span className="modalText"> First Name:</span>
-            <input type="text" required className="modalInput" />
+            <input
+              onChange={(e) => {
+                setUser((prev) => {
+                  return { ...prev, firstName: e.target.value };
+                });
+              }}
+              type="text"
+              required
+              className="modalInput"
+            />
           </label>
           <label>
             <span className="modalText"> Last Name:</span>
-            <input type="text" required className="modalInput" />
+            <input
+              onChange={(e) => {
+                setUser((prev) => {
+                  return { ...prev, lastName: e.target.value };
+                });
+              }}
+              type="text"
+              required
+              className="modalInput"
+            />
           </label>
           <label>
             <span className="modalText"> Age:</span>
-            <input type="number" required className="modalInput" />
+            <input
+              onChange={(e) => {
+                setUser((prev) => {
+                  return { ...prev, age: e.target.value };
+                });
+              }}
+              type="number"
+              required
+              className="modalInput"
+            />
           </label>
           <label>
             <span className="modalText"> From:</span>
-            <input type="text" required className="modalInput" />
+            <input
+              onChange={(e) => {
+                setUser((prev) => {
+                  return { ...prev, from: e.target.value };
+                });
+              }}
+              type="text"
+              required
+              className="modalInput"
+            />
           </label>
           <label>
             <span className="modalText"> Job:</span>
-            <input type="text" required className="modalInput" />
+            <input
+              onChange={(e) => {
+                setUser((prev) => {
+                  return { ...prev, job: e.target.value };
+                });
+              }}
+              type="text"
+              required
+              className="modalInput"
+            />
           </label>
           <div className="gender">
             <span className="modalText"> Gender:</span>
             <label>
-              <input type="radio" name="gender" value="male" required />
+              <input
+                onChange={(e) => {
+                  setUser((prev) => {
+                    return { ...prev, gender: e.target.value };
+                  });
+                }}
+                type="radio"
+                name="gender"
+                value="male"
+                required
+              />
               Male
             </label>
             <label>
-              <input type="radio" name="gender" value="female" required />
+              <input
+                onChange={(e) => {
+                  setUser((prev) => {
+                    return { ...prev, gender: e.target.value };
+                  });
+                }}
+                type="radio"
+                name="gender"
+                value="female"
+                required
+              />
               Female
             </label>
           </div>
