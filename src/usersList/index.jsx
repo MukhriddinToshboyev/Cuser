94989c865f889b7353dbc047a8700ae8555af181
Muxriddin Toshboyev <167794484+MukhriddinToshboyev@users.noreply.github.com>
2026-01-26 21:28:54 +0500

    style: update userCard class naming and enhance user display with age information

--- src/usersList/index.jsx
+++ src/usersList/index.jsx
@@ -5,16 +5,20 @@ function Users({ users, deleteUser }) {
     <div className="userList">
       {users.map((user) => {
         return (
-          <div key={user.id} className="user-card">
+          <div key={user.id} className="userCard">
             <img src={user.img} />
             <h3>
-              {user.firstName} {user.lastName}
+              {user.firstName} {user.lastName} {user.age} age
             </h3>
-            <p>{user.age} age</p>
             <p>From: {user.from}</p>
             <p>Job: {user.job}</p>
             <p>gender: {user.gender}</p>
-            <button onClick={() => deleteUser(user.id)}>Delete</button>
+            <button
+              className="userCardDelete"
+              onClick={() => deleteUser(user.id)}
+            >
+              Delete
+            </button>
           </div>
         );
       })}
