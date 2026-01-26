1bda863eb59d6733f28ed0a1b42ec9edcccca709
Muxriddin Toshboyev <167794484+MukhriddinToshboyev@users.noreply.github.com>
2026-01-26 16:04:09 +0500

    style: update Navbar title styling and adjust modal width and button layout

--- src/navbar/index.jsx
+++ src/navbar/index.jsx
@@ -3,7 +3,7 @@ function Navbar({ user }) {
   return (
     <div className="Navbar-container">
       <h1 className="Navbar-title">Cuser</h1>
-      <h1>
+      <h1 className="Navbar-title">
         {" "}
         {user.length > 0 ? "You have:" + " " + user.length : "No users :("}
       </h1>
